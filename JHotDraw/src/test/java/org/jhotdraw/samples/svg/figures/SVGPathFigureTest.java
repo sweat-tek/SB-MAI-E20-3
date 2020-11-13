25eaaee1e6497aab468092e86dd2a9a9024c1df5
do-nat <47325790+do-nat@users.noreply.github.com>
2020-11-13 17:53:17 +0100

    Check for calls of getChildren with spy

--- JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/SVGPathFigureTest.java
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/SVGPathFigureTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
+import java.util.Collections;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -22,35 +23,33 @@ public class SVGPathFigureTest {
 
     @Before
     public void setUp() {
-        svgPathFigure = new SVGPathFigure();
+        svgPathFigure = spy(SVGPathFigure.class);
         child = mock(SVGBezierFigure.class);
         view = mock(DrawingView.class);
         point2D = mock(Point2D.Double.class);
         evt = mock(MouseEvent.class);
         when(view.getHandleDetailLevel()).thenReturn(2);
         when(evt.getClickCount()).thenReturn(2);
-    }
-
-    private double getTolerance() {
-        return 5f / view.getScaleFactor();
+        when(svgPathFigure.getChildren()).thenReturn(Collections.singletonList(child));
     }
 
     @Test
     public void handleMouseClickWithOnlyOneClick() {
         when(evt.getClickCount()).thenReturn(1);
         assertFalse(svgPathFigure.handleMouseClick(point2D, evt, view));
-        verify(child, never()).findSegment(point2D, svgPathFigure.getToleranceFor(view));
+        verify(svgPathFigure, never()).getChildren();
     }
 
     @Test
     public void handleMouseClickWithHandleLevelNotDivisibleBy2() {
         when(evt.getClickCount()).thenReturn(1);
         assertFalse(svgPathFigure.handleMouseClick(point2D, evt, view));
-        verify(child, never()).findSegment(point2D, svgPathFigure.getToleranceFor(view));
+        verify(svgPathFigure, never()).getChildren();
     }
 
     @Test
     public void handleMouseClickWithNoChildren() {
+        when(svgPathFigure.getChildren()).thenReturn(Collections.emptyList());
         assertFalse(svgPathFigure.handleMouseClick(point2D, evt, view));
         verify(child, never()).findSegment(point2D, svgPathFigure.getToleranceFor(view));
     }
@@ -58,7 +57,7 @@ public class SVGPathFigureTest {
     @Test
     public void handleMouseClickWithOneChildAndNotFoundIndex() {
         svgPathFigure.add(0, child);
-        when(child.findSegment(point2D, this.getTolerance())).thenReturn(-1);
+        when(child.findSegment(point2D, svgPathFigure.getToleranceFor(view))).thenReturn(-1);
         assertFalse(svgPathFigure.handleMouseClick(point2D, evt, view));
         verify(child, times(1)).findSegment(point2D, svgPathFigure.getToleranceFor(view));
         verify(evt, never()).consume();
@@ -68,7 +67,7 @@ public class SVGPathFigureTest {
     public void handleMouseClickWithOneChildAndRegularIndex() {
         svgPathFigure.add(0, child);
         assertTrue(svgPathFigure.handleMouseClick(point2D, evt, view));
-        when(child.findSegment(point2D, this.getTolerance())).thenReturn(0);
+        when(child.findSegment(point2D, svgPathFigure.getToleranceFor(view))).thenReturn(0);
         verify(child, times(1)).findSegment(point2D, svgPathFigure.getToleranceFor(view));
         verify(evt, times(1)).consume();
         verify(child, times(1)).handleMouseClick(point2D, evt, view);
